ace109f56c380be6d14175eba548b9cf88844be0
Scott Shawcroft <scott.shawcroft@gmail.com>
2017-04-10 17:27:50 -0700

    Update the examples for busio.

--- examples/servo.py
+++ examples/servo.py
@@ -1,17 +1,14 @@
 import time
 
 from board import *
-# Use this import for ESP8266 and other boards with software I2C interfaces:
-import bitbangio as io
-# Or use this import for SAMD21 and boards with a native hardware I2C interace:
-#import nativeio as io
+import busio
 
 # Import the PCA9685 servo module.
 from adafruit_pca9685 import servo
 
 
 # Create the I2C bus interface.
-i2c = io.I2C(SCL, SDA)
+i2c = busio.I2C(SCL, SDA)
 
 # Create a simple PCA9685 class instance.
 servos = servo.Servos(i2c)
--- examples/simpletest.py
+++ examples/simpletest.py
@@ -1,15 +1,11 @@
 from board import *
-# Use this import for ESP8266 and other boards with software I2C interfaces:
-import bitbangio as io
-# Or use this import for SAMD21 and boards with a native hardware I2C interace:
-#import nativeio as io
+import busio
 
 # Import the PCA9685 module.
 from adafruit_pca9685 import pca9685
 
-
 # Create the I2C bus interface.
-i2c = io.I2C(SCL, SDA)
+i2c = busio.I2C(SCL, SDA)
 
 # Create a simple PCA9685 class instance.
 pca = pca9685.PCA9685(i2c)
