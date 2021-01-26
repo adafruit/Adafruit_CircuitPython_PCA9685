57adf7cd5fa40e486ba8a33b2c9c2db9aea7093e
Javier <javiervalencia80@gmail.com>
2021-01-26 20:28:28 +0000

    exchanged import order

--- examples/pca9685_servo.py
+++ examples/pca9685_servo.py
@@ -8,8 +8,8 @@ import busio
 
 # Import the PCA9685 module. Available in the bundle and here:
 #   https://github.com/adafruit/Adafruit_CircuitPython_PCA9685
-from adafruit_pca9685 import PCA9685
 from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
 
 i2c = busio.I2C(SCL, SDA)
 
